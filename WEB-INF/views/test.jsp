8aff37056e301af3f15e326347e99a8855aefa80
kingdo <1440852110@qq.com>
2017-07-19 23:18:54 +0800

    scanQRcode

--- WEB-INF/views/test.jsp
+++ WEB-INF/views/test.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%--
   Created by IntelliJ IDEA.
   User: kingdo
@@ -6,15 +7,18 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
 <html>
 <head>
     <title>Title</title>
-    <script>
-        alert(new Date().getTime());
-
-    </script>
+    <meta name="viewport" content="initial-scale=1.0,user-scalable=no,maxinum-scale=1,width=device-width">
+    <script type="text/javascript" rel="script" src="<c:url value="/static/plugins/jquery-3.2.1.min.js"/>"></script>
+    <script type="text/javascript" rel="script" src="<c:url value="http://res.wx.qq.com/open/js/jweixin-1.2.0.js"/>"></script>
+    <script type="text/javascript" rel="script" src="<c:url value="/static/until/sha1.js"/>"></script>
+    <script type="text/javascript" rel="script" src="<c:url value="/static/until/random.js"/>"></script>
+    <script type="text/javascript" rel="script" src="<c:url value="/static/wechat/wechat.js"/>"></script>
 </head>
 <body>
-
+<button onclick="scanQRCode()">嫂嫂</button>
 </body>
 </html>
--- static/wechat/wechat.js
+++ static/wechat/wechat.js
@@ -5,37 +5,29 @@
 $(function () {
     let nonceStr=randomWord();
     let timestamp=new Date().getTime();
-    let ticket;
-    let signature;
     $.ajax({
         type: 'get',
-        url: "/wechat/getAccessToken",
+        url: "/wechat/getSignature?nonceStr="+nonceStr+"&timestamp="+timestamp,
         dataType: 'text',
-        success: function (accessToken) {
-            $.ajax({
-                type: 'get',
-                url: "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token="+accessToken+"&type=jsapi",
-                dataType: 'json',
-                success: function (returnData) {
-                    ticket=returnData['ticket'];
-                    signature=hex_sha1("jsapi_ticket="+ticket+ "&timestamp="+timestamp+"&url=http://mp.weixin.qq.com?params=value");
-                    wx.config({
+        success: function (signature) {
+            wx.config({
 
-                        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 
-                        appId: 'wx7495c0e23819716a', // 必填，公众号的唯一标识
+                appId: 'wx7495c0e23819716a', // 必填，公众号的唯一标识
 
-                        timestamp: timestamp, // 必填，生成签名的时间戳
+                timestamp: timestamp, // 必填，生成签名的时间戳
 
-                        nonceStr: nonceStr, // 必填，生成签名的随机串
+                nonceStr: nonceStr, // 必填，生成签名的随机串
 
-                        signature: signature,// 必填，签名，见附录1
+                signature: signature,// 必填，签名，见附录1
 
-                        jsApiList: ['scanQRCode','chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+                jsApiList: ['scanQRCode','chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
 
-                    });
-                }
             });
+        },
+        error:function (xhr) {
+            $('html').html(xhr.responseText);
         }
     });
 });
