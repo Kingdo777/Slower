c3758d62283d2a3221aab62c8d67b04a3108f500
kingdo <1440852110@qq.com>
2017-07-26 10:50:02 +0800

    avatar

--- static/wechat/wechat.js
+++ static/wechat/wechat.js
@@ -91,7 +91,7 @@ function getAvatar() {
 
             let localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
             wx.uploadImage({
-                localId: localIds, // 需要上传的图片的本地ID，由chooseImage接口获得
+                localId: localIds.toString(), // 需要上传的图片的本地ID，由chooseImage接口获得
                 isShowProgressTips: 1, // 默认为1，显示进度提示
                 success: function (res) {
                     setTimeout(function () {
