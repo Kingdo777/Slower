10204c8c46c846ed9bfdebe2525d269b579c0ca9
kingdo <1440852110@qq.com>
2017-07-26 09:46:27 +0800

    avatar

--- static/wechat/wechat.js
+++ static/wechat/wechat.js
@@ -12,7 +12,7 @@ $(function () {
         success: function (signature) {
             wx.config({
 
-                debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 
                 appId: 'wx7495c0e23819716a', // 必填，公众号的唯一标识
 
@@ -79,12 +79,26 @@ function wxpay(money,oddNumber,callBackUrl,orderId) {
     });
 }
 function getAvatar() {
-    wx.getLocalImgData({
-        localId: '', // 图片的localID
+    wx.chooseImage({
+
+        count: 1, // 默认9
+
+        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+
+        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+
         success: function (res) {
-            let localData = res.localData; // localData是图片的base64数据，可以用img标签显示
-            upload(localData);
+
+            let localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+            wx.getLocalImgData({
+                localId: localIds, // 图片的localID
+                success: function (res) {
+                    let localData = res.localData; // localData是图片的base64数据，可以用img标签显示
+                    upload(localData);
+                }
+            });
         }
+
     });
 }
 function getBlobBydataURI(dataURI,type) {
